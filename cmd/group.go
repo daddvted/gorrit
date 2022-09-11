f4328a22a0580cb0277548617bee8a7d4c39f650
Ted <ski2per@gmail.com>
2022-09-12 00:39:54 +0800

    add group check command

--- cmd/group.go
+++ cmd/group.go
@@ -39,12 +39,29 @@ var groupListCmd = &cobra.Command{
 	Short: "List Gerrit groups",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("list gerrit groups")
+		fmt.Println("args", len(args))
+	},
+}
+
+var groupCheckCmd = &cobra.Command{
+	Use:   "check",
+	Short: "Show user's groups",
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("args", args)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(groupCmd)
 	groupCmd.AddCommand(groupListCmd)
+	groupCmd.AddCommand(groupCheckCmd)
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
@@ -54,6 +71,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// groupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-	groupCmd.Flags().StringVarP(&username, "username", "u", "", "Gerrit username(short name)")
+	// groupCmd.Flags().StringVarP(&username, "username", "u", "", "Gerrit username(short name)")
 
 }
