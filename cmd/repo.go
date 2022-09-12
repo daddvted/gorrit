070f3966240d12196ffaebabf8ba1ea69c6c8315
Ted <ski2per@gmail.com>
2022-09-12 15:07:23 +0800

    update

--- cmd/repo.go
+++ cmd/repo.go
@@ -10,31 +10,44 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// webCmd represents the web command
-var webCmd = &cobra.Command{
-	Use:   "web",
-	Short: "Run RESTful API for Gerrit",
+var manifestFile string
+
+// repoCmd represents the repo command
+var repoCmd = &cobra.Command{
+	Use:   "repo",
+	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
+	// Run: func(cmd *cobra.Command, args []string) {
+	// 	fmt.Println("repo called")
+	// },
+}
+
+var repoInfoCmd = &cobra.Command{
+	Use:   "info",
+	Short: "Show info from repo manifest",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("web called")
+		fmt.Println("show info from repo manifest")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(webCmd)
+	rootCmd.AddCommand(repoCmd)
+	repoCmd.AddCommand(repoInfoCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// webCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// repoCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// webCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// repoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	repoCmd.Flags().StringVarP(&manifestFile, "manifest", "x", "", "xxx.xml")
 }
